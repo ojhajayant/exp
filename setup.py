c4cb8850ac7261842ae364f704d7dcf0641ad0b2
JayO <45680293+ojhajayant@users.noreply.github.com>
2023-05-31 17:29:22 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='exp',
+    version='0.0.1',
+    description='My exp repo',
+    url='git@github.com:ojhajayant/exp.git',
+    author='Jayant Ojha',
+    author_email='jayantojha78@gmail.com',
+    license='unlicense',
+    packages=find_packages(),
+    zip_safe=False
+)
